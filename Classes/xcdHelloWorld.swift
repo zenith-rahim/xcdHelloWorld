f9cae191f75202675fbf32f0177188ea363226ba
optomate.io <abdul.rahim@optomate.io>
2022-06-09 16:57:38 +0530

    initial commit

---
+++ Classes/xcdHelloWorld.swift
@@ -0,0 +1,15 @@
+//
+//  xcdHelloWorld.swift
+//  xcdHelloWorld
+//
+//  Created by mac on 09/06/22.
+//
+
+import Foundation
+
+public class HelloWorld{
+    public func IoPrint(_ data: String) -> Void
+    {
+        print("hello ", data)
+    }
+}
---
+++ Example/Pods/Target Support Files/xcdHelloWorld/xcdHelloWorld-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_xcdHelloWorld : NSObject
+@end
+@implementation PodsDummy_xcdHelloWorld
+@end
